f23c0c429125e44353ee6099bf7cd2bc5195811d
ksw4015 <zealot4321@naver.com>
2024-12-31 20:38:57 +0900

    *프로그래머스 양궁대회 (TC 8, 18, 25 X)
    *프로그래머스 캐릭터 좌표 (풀이완)
    
    *프로그래머스 조이스틱 (풀이완 but 이해 잘 안됨)

--- src/dfs/backtracking/ArcheryContest.java
+++ src/dfs/backtracking/ArcheryContest.java
@@ -12,31 +12,44 @@ import java.util.Arrays;
 public class ArcheryContest {
     static ArrayList<int[]> result;
     static int diff;
+    static int max;
 
     public int[] solution(int n, int[] info) {
         result = new ArrayList<>();
         diff = 0;
+        max = 0;
         dfs(0, 0, n, info, new int[info.length]);
 
         if(result.size() == 0)
             return new int[]{-1};
 
-        int[] answer = result.get(0);
-
+        int[] answer = result.get(result.size()-1);
         for(int[] r : result) {
             for(int i = 10 ; i >= 0 ; i--) {
-                if (answer[i] > r[i]) {
-                    answer = r;
+                if (answer[i] > r[i] && checkScore(r, info)) {
+                    answer = r.clone();
                     break;
                 }
             }
         }
-
         return answer;
     }
 
+    private boolean checkScore(int[] a, int[] info) {
+        int score = 0;
+        for(int i = 0 ; i < info.length ; i++) {
+            if(a[i] == 0 && info[i] == 0)
+                continue;
+            int s = 10 - i;
+            if(a[i] > info[i]) {
+                score += s;
+            }
+        }
+        return score == max;
+    }
+
     private void dfs(int arrows, int start, int n, int[] info, int[] scores) {
-        if(arrows == n) {
+        if(arrows >= n) {
             int a = 0; // 라이언
             int b = 0; // 어피치
             for(int i = 0 ; i < info.length ; i++) {
@@ -50,18 +63,20 @@ public class ArcheryContest {
                 }
             }
             if(a > b && diff <= Math.abs(a-b)) {
+                if(max < a)
+                    max = a;
                 diff = Math.abs(a-b);
                 result.add(scores.clone());
             }
         } else {
             for(int i = start ; i < info.length ; i++) {
                 int arr = arrows + info[i] + 1;
-                // 화살 개수가 이미 넘어갈 때
-                // 남은거 다이겨도 질 때
-                if(arr > n)
-                    continue;
                 int next = i + 1;
-                scores[i] = info[i] + 1;
+                if(arr > n) {
+                    scores[i] = arr-n;
+                } else {
+                    scores[i] = info[i] + 1;
+                }
                 dfs(arr, next, n, info, scores);
                 scores[i] = 0;
             }
---
+++ src/simulation/CharacterCoordinate.java
@@ -0,0 +1,48 @@
+package simulation;
+
+import java.util.Arrays;
+
+/**
+ * 프로그래머스
+ *
+ */
+public class CharacterCoordinate {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+
+        int x = board[0] / 2;
+        int y = board[1] / 2;
+        for(int i = 0 ; i < keyinput.length ; i++) {
+            if(keyinput[i].equals("left")) {
+                if(answer[0] > -x) {
+                    answer[0]--;
+                }
+            } else if(keyinput[i].equals("right")) {
+                if(answer[0] < x) {
+                    answer[0]++;
+                }
+            } else if(keyinput[i].equals("up")) {
+                if(answer[1] < y) {
+                    answer[1]++;
+                }
+            } else {
+                if(answer[1] > -y) {
+                    answer[1]--;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CharacterCoordinate T = new CharacterCoordinate();
+        System.out.println(Arrays.toString(T.solution(
+                new String[]{"left", "right", "up", "right", "right"},
+                new int[]{11, 11}
+        )));
+        System.out.println(Arrays.toString(T.solution(
+                new String[]{"down", "down", "down", "down", "down"},
+                new int[]{7, 9}
+        )));
+    }
+}
