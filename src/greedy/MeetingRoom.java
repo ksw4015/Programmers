541069d2933769a5ab49a27b1c3aa25ffec2d765
ksw4015 <zealot4321@naver.com>
2025-01-04 15:02:48 +0900

    *그리디 알고리즘 강의 문제
    *씨름선수, 회의실

---
+++ src/greedy/MeetingRoom.java
@@ -0,0 +1,34 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MeetingRoom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] meetings = new int[n][2];
+
+        for(int i = 0 ; i < n ; i++) {
+            meetings[i][0] = sc.nextInt();
+            meetings[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(meetings, (i1, i2) -> {
+            if(i1[1] == i2[1])
+                return i1[0] - i2[0];
+            return i1[1] - i2[1];
+        });
+
+        int answer = 1;
+        int endTime = meetings[0][1];
+        for(int i = 1 ; i < n ; i++) {
+            if(meetings[i][0] >= endTime) {
+                answer++;
+                endTime = meetings[i][1];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ src/greedy/Wrestling.java
@@ -0,0 +1,39 @@
+package greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 기초강의
+ *
+ * 씨름선수
+ */
+public class Wrestling {
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] players = new int[n][2];
+        for(int i = 0 ; i < n ; i++) {
+            players[i][0] = in.nextInt();
+            players[i][1] = in.nextInt();
+        }
+
+        int answer = 1;
+        Arrays.sort(players, (o1, o2) -> {
+            if(o2[0] == o1[0])
+                return o2[1] - o1[1];
+            return o2[0] - o1[0];
+        });
+        int max = players[0][1];
+
+        for(int i = 1 ; i < players.length ; i++) {
+            if(max <= players[i][1]) {
+                answer++;
+                max = Math.max(max, players[i][1]);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
